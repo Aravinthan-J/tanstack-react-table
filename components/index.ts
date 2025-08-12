9835280dd45223eeaf720897cf2d580769736b0f
Aravinthan-J <aravinthanjai@gmail.com>
2025-08-12 19:14:20 +0530

    Components index added

---
+++ components/index.ts
@@ -0,0 +1,18 @@
+export { Table } from "./Table";
+export type {
+  TableProps,
+  TableRef,
+  ColumnProps,
+  TableEvent,
+  CellProps,
+  TableTheme,
+} from "./Table/Table.types";
+
+export {
+  EXPANDABLE_TYPES,
+  UPDATED_EVENTS,
+  CELL_TYPES,
+} from "./Table/utils/events";
+
+export * from "./Table/cells";
+export * from "./themes";
