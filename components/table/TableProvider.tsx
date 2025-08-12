ebc3f5a65cd02b3bb438b501df09752f9e5b057d
Aravinthan-J <aravinthanjai@gmail.com>
2025-08-12 19:16:02 +0530

    Table provider file

---
+++ components/table/TableProvider.tsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext } from "react";
+import type { Table } from "@tanstack/react-table";
+import type {
+  ExpandableProps,
+  TableRef,
+  TableTheme,
+  CellProps,
+  ExpandableProps,
+} from "./Table.types";
+
+interface TableContextValue {
+  table: Table<any>;
+  expandable: ExpandableProps;
+  rowHeight: number;
+  tableRef: TableRef;
+  theme?: TableTheme;
+  editableCellComponents: Record<string, React.ComponentType<CellProps>>;
+}
+
+const TableContext = createContext<TableContextValue | null>(null);
+
+export function TableProvider({
+  children,
+  ...props
+}: TableContextValue & { children: React.ReactNode }) {
+  return (
+    <TableContext.Provider value={props}>{children}</TableContext.Provider>
+  );
+}
+
+export function useTable() {
+  const context = useContext(TableContext);
+  if (!context) {
+    throw new Error("useTable must be used within TableProvider");
+  }
+  return context;
+}
