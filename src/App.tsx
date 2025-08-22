66b9e814f995f04527b856bc10d5658510a826d6
Aravinthan-J <aravinthanjai@gmail.com>
2025-08-22 15:04:40 +0530

    App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,18 @@ import { EditableTable } from "./table/examples/EditableTable.tsx";
 import { NestedRowsTable } from "./table/examples/NestedRowsTable.tsx";
 import { VirtualizedTable } from "./table/examples/VirtualizedTable.tsx";
 import { Table } from "./table/index.ts";
-import {
-  dummyColumn,
-  dummyData,
-  dummyLargeTheme,
-  dummyUserTheme,
-} from "./table/mock";
+import { dummyColumn, dummyData, dummyLargeTheme } from "./table/mock";
 
 import "./index.css";
 
 export function App() {
   return (
     <div className="flex flex-col gap-20 m-8 p-16 h-full">
-      <h1>All Example Tables</h1>
-      <section className="mb-8 h-400">
-        <h2 className="text-primary-500">Default Table</h2>
+      <h1 className="text-black font-bold text-5xl">All Example Tables</h1>
+      <section className="my-10 h-400 w-fit">
+        <h2 className="text-primary-500 my-10 font-bold text-2xl">
+          Default Table
+        </h2>
         <Table
           datasource={dummyData}
           columns={dummyColumn}
@@ -41,23 +38,31 @@ export function App() {
           }}
         />
       </section>
-      <section className="mb-8 mt-10  h-200">
-        <h2 className="text-primary-500">Basic Table</h2>
+      <section className="my-10 h-200 w-fit ">
+        <h2 className="text-primary-500 my-10 font-bold text-2xl">
+          Basic Table
+        </h2>
         <BasicTable />
       </section>
 
-      <section className="mb-8 mt-10  h-240">
-        <h2 className="text-primary-500">Nested Rows Table</h2>
+      <section className="my-10 h-240 w-fit">
+        <h2 className="text-primary-500 my-10 font-bold text-2xl">
+          Nested Rows Table
+        </h2>
         <NestedRowsTable />
       </section>
 
-      <section className="mb-8 mt-10  h-800">
-        <h2 className="text-primary-500">Virtualized Table</h2>
+      <section className="my-10 h-800 w-fit">
+        <h2 className="text-primary-500 my-10 font-bold text-2xl">
+          Virtualized Table
+        </h2>
         <VirtualizedTable />
       </section>
 
-      <section className="mb-8 mt-10  h-300">
-        <h2 className="text-primary-500">Editable Table</h2>
+      <section className="my-10 h-300 w-fit">
+        <h2 className="text-primary-500 my-10 font-bold text-2xl">
+          Editable Table
+        </h2>
         <EditableTable />
       </section>
     </div>
